997d3748439018d6ddf0a7e8696ffc81fb347cc7
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2021-12-27 14:26:44 +0300

    Add project files.

---
+++ Entity/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Category
+    {   [Key]
+        public int CategoryId { get; set; }
+        public int CategoryName { get; set; }
+    }
+}
---
+++ Entity/Concrete/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Task
+    {   [Key]
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string CategoryName { get; set; }
+        public string TaskDetails { get; set; }
+        public DateTime? TaskDate { get; set; }
+    }
+}
