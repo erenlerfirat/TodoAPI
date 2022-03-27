30566d186f9856e2b3e72b0928ad0421318af37f
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2022-03-27 18:42:41 +0300

    Result Return Type Added

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorDataResult<T>:DataResult<T>
+    {
+        public ErrorDataResult(T data,bool success , string message) : base(success , message , data)
+        {
+
+        }
+        public ErrorDataResult(T data, bool success) : base(success , data)
+        {
+
+        }
+        public ErrorDataResult(string message) : base(false , message , default)
+        {
+
+        }
+        public ErrorDataResult() : base (false , default)
+        {
+
+        }
+    }
+}
--- TodoAPI/Controllers/TodoController.cs
+++ TodoAPI/Controllers/TodoController.cs
@@ -1,14 +1,11 @@
 ﻿using Business.Abstract;
 using Business.Concrete;
 using Business.Constants;
-using DataAccess.Concrete.EntityFramework;
 using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TodoAPI.Controllers
 {
