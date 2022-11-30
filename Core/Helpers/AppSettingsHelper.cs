0f0664f77ec4232cef61b47f6749565aecd23263
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2022-11-30 21:58:30 +0300

    Appsettings Helper &JWT Middleware :anchor:

---
+++ Core/Helpers/AppSettingsHelper.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Core.Helpers
+{
+    public static class AppSettingsHelper
+    {   
+        private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json").Build();
+
+        public static T GetValue<T>(string key, T defaultValue)
+        {            
+            var value = Configuration.GetValue<T>(key, defaultValue);
+
+            if (string.IsNullOrEmpty(value.ToString()))
+                return defaultValue;
+
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+
+}
