c52bdbe34f7957d0ffa35a50b286ce7b21c94365
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2022-12-03 22:04:46 +0300

    JWT Generator Helper Add :100:

---
+++ Core/Helpers/TokenHelper.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entity.Concrete;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Helpers
+{
+    public static class TokenHelper
+    {
+        public static string GenerateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(AppSettingsHelper.GetValue("secret", ""));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
