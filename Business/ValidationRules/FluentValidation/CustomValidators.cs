1b4b34b4bac7a2503923fb34700054694594989f
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2023-03-11 19:22:00 +0300

    FluentValidation rename :100:

--- Business/ValidationRules/FluentValidation/CustomValidators.cs
+++ Business/ValidationRules/FluentValidation/CustomValidators.cs
@@ -1,11 +1,6 @@
 ﻿using Entity.Concrete;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Business.ValidationRules.FluentValidation
 {
