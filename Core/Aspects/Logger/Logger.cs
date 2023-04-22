45808c713b6d791b02774b94a2a171e2f09577fc
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2023-04-22 22:39:06 +0300

    Delete unused method :recycle:

--- Core/Aspects/Logger/Logger.cs
+++ Core/Aspects/Logger/Logger.cs
@@ -1,10 +1,6 @@
-﻿using Castle.Core.Configuration;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using NLog;
-using NLog.Config;
 using NLog.Extensions.Logging;
-using NLog.Targets;
-using System;
 using System.IO;
 using ILogger = NLog.ILogger;
 
@@ -21,31 +17,6 @@ namespace Core.Aspects.Log
             LogManager.Configuration = new NLogLoggingConfiguration(Configuration.GetSection("NLog"));
             _logger = LogManager.GetCurrentClassLogger();
         }
-        //private void FormatLogConfig()
-        //{
-        //    LogManager.Configuration = new NLogLoggingConfiguration(Configuration.GetSection("NLog"));
-        //    var configuration = new LoggingConfiguration();
-        //    var target = new FileTarget();
-        //    string fullDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffffff");
-        //    string today = DateTime.Now.ToString("yyyy-MM-dd");
-        //    string currentHour = DateTime.Now.ToString("yyyy-MM-ddTHH");
-        //    string assembly = typeof(T).Assembly.GetName().Name;
-        //    string currentClass = typeof(T).Name;
-
-        //    target.FileName = "c:\\Logs\\TODO\\" + assembly + "\\" + today + "\\" + currentHour + ".log";
-
-        //    target.Layout = "[${level:uppercase=true}] " + fullDate + "  " + currentClass + " : ${message} ${exception:format=tostring}";
-
-        //    var rule = new LoggingRule("*", LogLevel.Trace, target);
-
-        //    configuration.LoggingRules.Add(rule);
-
-        //    configuration.AddTarget("BaseFile", target);
-
-        //    LogManager.ReconfigExistingLoggers();
-
-        //    LogManager.Configuration = configuration;
-        //}
 
         public void Error(string message)
         {
